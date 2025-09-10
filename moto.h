758e1006dbe0569faeebdaf1c987c0442ee46f4a
l3nny56 <lenny2005.56@gmai.com>
2025-09-10 17:22:20 +0200

    Ajoutez des fichiers projet.

---
+++ moto.h
@@ -0,0 +1,33 @@
+/*****************************************************************//**
+ * \file   moto.h
+ * \brief  Declaration de la classe moto
+ * 
+ * \author Pierre
+ * \date   June 2023
+ *********************************************************************/
+
+#ifndef moto_H
+#define moto_H
+#include <iostream>
+#include <string>
+
+
+
+class moto
+{
+
+    private:
+  
+
+
+
+    protected : 
+
+
+
+    public:
+
+       
+};
+
+#endif
