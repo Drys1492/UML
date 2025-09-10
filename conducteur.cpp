52224d4743c8aea2aa6e361a4c0193ccf1556835
l3nny56 <lenny2005.56@gmai.com>
2025-09-10 17:31:36 +0200

    ajout moto.cpp et conducteur.cpp

---
+++ conducteur.cpp
@@ -0,0 +1,33 @@
+/*****************************************************************//**
+ * \file   conducteur.h
+ * \brief  Declaration de la classe conducteur
+ * 
+ * \author Pierre
+ * \date   June 2023
+ *********************************************************************/
+
+#ifndef conducteur_H
+#define conducteur_H
+#include <iostream>
+#include <string>
+
+
+
+class conducteur
+{
+
+    private:
+        
+
+
+
+    protected : 
+
+
+
+    public:
+        
+       
+};
+
+#endif
