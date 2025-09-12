4c1b67172b67fb55eec8901a5e52ab1e24a37acb
l3nny56 <lenny2005.56@gmai.com>
2025-09-12 10:51:29 +0200

    modification main_moto_conducteur.cpp

---
+++ main_moto_conducteur.cpp
@@ -0,0 +1,16 @@
+#include <iostream> // Bibliothèque pour afficher du texte
+#include "moto.h"
+#include "conducteur.h"
+
+
+int main() {
+
+    Moto maMoto;
+
+
+    std::cout << "Test" << std::endl; // Affiche Hello, World!
+
+    Conducteur Conducteur_1;
+    std::cout << "Test_2" << std::endl;
+    return 0;
+}
